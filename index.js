bd368141585280cb3d6a441cbe58a734bd0ffb8b
David Yim <yimd85@gmail.com>
2017-10-20 17:21:43 -0400

    updates based on James comments

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('postgres://yimd85@localhost:5432/bulletinboard');
+
+var sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 5432,
+  dialect: "postgres",
+});
+
+//I hide the db url. See lines 3-10
+// var sequelize = new Sequelize('postgres://yimd85@localhost:5432/bulletinboard');
+
+
+
 
 var express = require('express');
 var path = require('path')
@@ -15,8 +29,40 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // app.use(bodyParser.json());
 
 var Stuff = sequelize.define('boardpost', {
-  user: Sequelize.STRING(100),
-  post: Sequelize.TEXT
+
+
+
+
+  title: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        validate: {
+			       notEmpty: true
+         }
+  },
+  body: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        validate: {
+             notEmpty: true
+         }
+  },
+  //i've renamed the fields to be called title and body
+  //i've created 2 validation paramsters to with argurements so that it is a required field.
+  //see lines 36-49
+
+
+
+  // user: Sequelize.STRING(100),
+  // post: Sequelize.TEXT,
+
+
+  created: {
+        type: Sequelize.DATE(3),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)'),
+        field: 'created_at'
+    }//i've created the create_at field. See lines 56-59
+
 });
 
 //creating first entry
@@ -24,8 +70,8 @@ Stuff
   .sync()
     .then(function(){
       Stuff.create({
-          user: 'bobo',
-          post: 'first post and screw hello world!'
+          title: 'bobo',
+          body: 'first post and screw hello world!'
       });
   })
 
@@ -46,8 +92,8 @@ app.post('/addshit',function(request,response){
     .findAll()
       .then(function(){
         Stuff.create({
-            user: request.body.user,
-            post: request.body.post
+            title: request.body.user,
+            body: request.body.post
         });
         response.redirect('/')
   })
